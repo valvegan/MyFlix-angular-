b3d64c9a103af469fb70e4d5e78a47f9be03d089
valvegan <valentinavaccari@hotmail.com>
2022-06-22 18:51:10 +0100

    6.3 done
    login form done

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,50 @@
+// src/app/user-registration-form/user-registration-form.component.ts
+import { Component, OnInit, Input } from '@angular/core';
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { username: '', password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  // This is the function responsible for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData
+      .userLogin(this.userData.username, this.userData.password)
+      .subscribe(
+        (result) => {
+          //setting the user data to local storage
+          localStorage.setItem('user', result.user.username);
+          localStorage.setItem('token', result.token);
+          // Logic for a successful sign in  goes here! (To be implemented)
+          this.dialogRef.close(); // This will close the modal on success!
+          console.log(result);
+          this.snackBar.open(result, 'OK', {
+            duration: 2000,
+          });
+        },
+        (result) => {
+          console.log(result);
+          this.snackBar.open(result, 'OK', {
+            duration: 2000,
+          });
+        }
+      );
+  }
+}
